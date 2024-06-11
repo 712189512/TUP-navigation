e71038e0cc4049fcf1c8ab07c9416ca238b0355e
712189512 <gabrielmaraon.gm@gmail.com>
2024-06-11 10:42:05 +0800

    change source of initial video

--- script.js
+++ script.js
@@ -70,8 +70,8 @@ $(document).ready(function() {
   // Load the intro video then the looping video
   const initialVideo = videoSelector.value;
   const videoElement = document.getElementById('video-background');
-  videoElement.src = "assets\\INTRO AREAL VIEW0034-0150.mp4";
-  // videoElement.src = "https://github.com/712189512/TUP-navigation/raw/main/assets/INTRO%20AREAL%20VIEW0034-0150.mp4";
+  // videoElement.src = "assets\\INTRO AREAL VIEW0034-0150.mp4";
+  videoElement.src = "https://github.com/712189512/TUP-navigation/sraw/main/assets/INTRO%20AREAL%20VIEW0034-0150.mp4";
   videoElement.type = "video/mp4"
   videoElement.load();
   loadingScreen.style.display = 'flex';
